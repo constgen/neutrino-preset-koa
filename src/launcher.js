2b2b6588da8ccd581819d8aae0efcd44c391c5dc
constgen <const.gen@gmail.com>
2018-04-28 16:11:23 +0300

    Add graceful termination

--- src/launcher.js
+++ src/launcher.js
@@ -1,3 +1,5 @@
+import { setTimeout } from 'timers';
+
 'use strict';
 
 let http = require('http');
@@ -6,6 +8,7 @@ let http = require('http');
 
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
+const KILL_TIMEOUT = 10 * 1000;
 let app = require('__entry__');
 let currentApp = app.callback();
 let server = http.createServer(currentApp).listen({port: PORT, host: HOST}, function (err) {
@@ -37,3 +40,43 @@ if (module.hot) {
         server.close();
     });
 }
+
+// http://glynnbird.tumblr.com/post/54739664725/graceful-server-shutdown-with-nodejs-and-express
+// if (process.platform === 'win32') {
+//     require('readline').createInterface({
+//         input: process.stdin,
+//         output: process.stdout
+//     }).on('SIGINT', function () {
+//         process.emit('SIGINT');
+//     });
+// }
+// process.title = ''
+// process.stdin.resume();
+
+function close(){
+	console.log('Closing...');
+	server.close(function(){
+		console.log('Server closed');
+		// process.exit();
+	});
+	process.exitCode = 0
+	setTimeout(function(){
+		console.log('Server killed, due to timeout');
+		process.exit(1)
+	}, KILL_TIMEOUT)
+}
+function handleExit(){
+	console.log('Server exited');
+}
+
+
+// Ctrl + C
+process.once('SIGINT', close);
+// Kill process
+process.once('SIGTERM', close);
+// Ctrl + Break
+process.once('SIGBREAK', close);
+// close terminal
+process.once('SIGHUP', close);
+
+process.once('exit', handleExit);
