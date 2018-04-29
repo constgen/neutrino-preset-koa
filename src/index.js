0f7daa5d3b47cdeecf251ba19c6d46218e24a2db
constgen <const.gen@gmail.com>
2018-04-30 02:00:32 +0300

    Add ESLint

--- src/index.js
+++ src/index.js
@@ -1,67 +1,59 @@
-'use strict';
-
 let path = require('path');
+
 let node = require('@neutrinojs/node');
 const clean = require('@neutrinojs/clean');
-let { EnvironmentPlugin, DefinePlugin } = require('webpack');
+let { DefinePlugin } = require('webpack');
 
 module.exports = function (neutrino, settings = {}) {
-    const NODE_MODULES = path.resolve(__dirname, '../node_modules');
+	const NODE_MODULES = path.resolve(__dirname, '../node_modules');
 	const LAUNCHER_PATH = path.resolve(__dirname, './launcher.js');
-	let devRun =  process.env.NODE_ENV === 'development'
+	let devRun = process.env.NODE_ENV === 'development';
 	let useLauncher = (settings.server !== undefined) ? Boolean(settings.server) : true;
-	let port = settings.server && Number(settings.server.port)
-	let defaultPort = devRun ? 0 : 80
+	let port = settings.server && Number(settings.server.port);
+	let defaultPort = devRun ? 0 : 80;
 
-    settings = {
-        node: settings.node || process.versions.node,
-        server: {
-            port: (port || port === 0) ? port : defaultPort
-        }
-    };
-	
-    neutrino.use(node, {
-        hot: true,
-        targets: {
-            node: settings.node
-        }
-    });
-    neutrino.use(clean, {
-        paths: [neutrino.options.output]
-    });
+	settings.node = settings.node || process.versions.node;
+	settings.server = {
+		port: (port || port === 0) ? port : defaultPort
+	};
 
-    if (!useLauncher) return;
+	neutrino.use(node, {
+		hot: true,
+		targets: {
+			node: settings.node
+		}
+	});
+	neutrino.use(clean, {
+		paths: [neutrino.options.output]
+	});
 
-    Object.keys(neutrino.options.mains).forEach(function (key) {
-        neutrino.config
-            .entry(key)
-                .clear()
+	if (!useLauncher) return;
+
+	Object.keys(neutrino.options.mains).forEach(function (key) {
+		neutrino.config
+			.entry(key)
+				.clear()
 				.add(LAUNCHER_PATH)
-				.when(devRun, function(entry){
-					entry.add(`${require.resolve('webpack/hot/poll')}?1000`)
+				.when(devRun, function (entry) {
+					entry.add(`${require.resolve('webpack/hot/poll')}?1000`);
 				})
-                .end()
-            .resolve.alias
-                .set('__entry__', require.resolve(neutrino.options.mains[key]))
-                .end().end();
-    });
+				.end()
+			.resolve.alias
+				.set('__entry__', require.resolve(neutrino.options.mains[key]))
+				.end().end();
+	});
 
-    neutrino.config
-        .resolve.modules
-            .add(NODE_MODULES)
-            .end().end()
-        .resolveLoader.modules
-            .add(NODE_MODULES)
-            .end().end()
-        .plugin('env')
-            .use(EnvironmentPlugin, [{
-                // PORT: settings.server.port
-            }])
-            .end()
-        .plugin('define')
-            .use(DefinePlugin, [{
-                'process.env.PORT': `process.env.PORT || ${settings.server.port}`,
-                'process.env.HOST': `process.env.HOST || '0.0.0.0'`
-            }])
-            .end();
+	neutrino.config
+		.resolve.modules
+			.add(NODE_MODULES)
+			.end().end()
+		.resolveLoader.modules
+			.add(NODE_MODULES)
+			.end().end()
+		.plugin('define')
+			.use(DefinePlugin, [{
+				'process.env.PORT': `process.env.PORT || ${settings.server.port}`,
+				'process.env.HOST': `process.env.HOST || '0.0.0.0'`
+			}])
+			.end();
 };
--- src/launcher.js
+++ src/launcher.js
@@ -1,47 +1,46 @@
-import { setTimeout } from 'timers';
-
-'use strict';
-
 let http = require('http');
+
 let chalk = require('chalk');
+let app = require('__entry__');
+
 let ip = require('./ip');
 
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
-const KILL_TIMEOUT = 10 * 1000;
-let app = require('__entry__');
+const KILL_TIMEOUT = 9 * 1000;
+const KILL_SIGNALS = ['SIGINT', 'SIGTERM', 'SIGBREAK', 'SIGHUP'];
 let currentApp = app.callback();
-let server = http.createServer(currentApp).listen({port: PORT, host: HOST}, function (err) {
-    if (err) {
-        throw err;
-    }
-    else {
-        let { port, address } = this.address();
-		let protocol = this.addContext ? 'https' : 'http';
-		let ips = ip.isLocal(HOST) ? ip.locals : ip.all;
-		let message = `${chalk.blue('Server started on')}: ${ips.map(function(ip){
-			return chalk.green(`${protocol}://${ip}${port !== 80 ? `:${port}` : ''}`);
+let server = http.createServer(currentApp).listen({ port: PORT, host: HOST }, function (err) {
+	if (err) {
+		throw err;
+	}
+	else {
+		let { port, address } = server.address();
+		let protocol = server.addContext ? 'https' : 'http';
+		let ips = ip.isLocal(address) ? ip.locals : ip.all;
+		let message = `${chalk.blue('Server started on')}: ${ips.map(function (host) {
+			return chalk.green(`${protocol}://${host}${port !== 80 ? `:${port}` : ''}`);
 		}).join(', ')}`;
+
 		console.log(message);
-    }
+	}
 });
 
-if (module.hot) {
-    module.hot.accept('__entry__', function () {
-        try {
-            server.removeListener('request', currentApp);
-            app = require('__entry__');
-            currentApp = app.callback();
-            server.on('request', currentApp);
-        } catch (err) {
-			console.error(chalk.red(err));
-        }
-    });
-    module.hot.accept();
-    module.hot.dispose(function () {
-        console.log(chalk.blue('Server stopped. Restarting...'));
-        server.close();
-    });
+function close () {
+	console.log(chalk.blue('Server shutting down...'));
+	server.close(function () {
+		console.log(chalk.blue('Server closed'));
+		process.exit();
+	});
+
+	// process.exitCode = 0
+	setTimeout(function () {
+		console.log(chalk.red('Server killed, due to timeout'));
+		process.exit(1);
+	}, KILL_TIMEOUT);
+}
+function handleExit () {
+	console.log(chalk.yellow('Application exited'));
 }
 
 // http://glynnbird.tumblr.com/post/54739664725/graceful-server-shutdown-with-nodejs-and-express
@@ -56,30 +55,31 @@ if (module.hot) {
 // process.title = ''
 // process.stdin.resume();
 
-function close(){
-	console.log('Closing...');
-	server.close(function(){
-		console.log('Server closed');
-		// process.exit();
-	});
-	process.exitCode = 0
-	setTimeout(function(){
-		console.log('Server killed, due to timeout');
-		process.exit(1)
-	}, KILL_TIMEOUT)
-}
-function handleExit(){
-	console.log('Server exited');
-}
 
-
-// Ctrl + C
-process.once('SIGINT', close);
-// Kill process
-process.once('SIGTERM', close);
-// Ctrl + Break
-process.once('SIGBREAK', close);
-// close terminal
-process.once('SIGHUP', close);
+KILL_SIGNALS.forEach(function (signal) {
+	process.once(signal, close);
+});
 
 process.once('exit', handleExit);
+
+if (module.hot) {
+	module.hot.accept('__entry__', function () {
+		try {
+			server.removeListener('request', currentApp);
+			app = require('__entry__');
+			currentApp = app.callback();
+			server.on('request', currentApp);
+		} catch (err) {
+			console.error(chalk.red(err));
+		}
+	});
+	module.hot.accept();
+	module.hot.dispose(function () {
+		KILL_SIGNALS.forEach(function (signal) {
+			process.removeListener(signal, close);
+		});
+		process.removeListener('exit', handleExit);
+		console.log(chalk.blue('Server stopped. Restarting...'));
+		server.close();
+	});
+}
