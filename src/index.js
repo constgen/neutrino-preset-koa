1fe4753ff64d85fe2af9e6615c4a686afdcca096
constgen <const.gen@gmail.com>
2018-04-30 01:10:31 +0300

    Discover free port automatically

--- src/index.js
+++ src/index.js
@@ -7,21 +7,20 @@ let { EnvironmentPlugin, DefinePlugin } = require('webpack');
 
 module.exports = function (neutrino, settings = {}) {
     const NODE_MODULES = path.resolve(__dirname, '../node_modules');
-    const LAUNCHER_PATH = path.resolve(__dirname, './launcher.js');
+	const LAUNCHER_PATH = path.resolve(__dirname, './launcher.js');
+	let devRun =  process.env.NODE_ENV === 'development'
 	let useLauncher = (settings.server !== undefined) ? Boolean(settings.server) : true;
 	let port = settings.server && Number(settings.server.port)
+	let defaultPort = devRun ? 0 : 80
 
     settings = {
         node: settings.node || process.versions.node,
         server: {
-            port: (port || port === 0) ? port : 80
+            port: (port || port === 0) ? port : defaultPort
         }
     };
-
+	
     neutrino.use(node, {
-        //https
-        //http2
-        //get-port
         hot: true,
         targets: {
             node: settings.node
@@ -37,8 +36,10 @@ module.exports = function (neutrino, settings = {}) {
         neutrino.config
             .entry(key)
                 .clear()
-                .add(LAUNCHER_PATH)
-                .add(`${require.resolve('webpack/hot/poll')}?1000`)
+				.add(LAUNCHER_PATH)
+				.when(devRun, function(entry){
+					entry.add(`${require.resolve('webpack/hot/poll')}?1000`)
+				})
                 .end()
             .resolve.alias
                 .set('__entry__', require.resolve(neutrino.options.mains[key]))
