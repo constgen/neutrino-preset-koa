e1f0a4f6fb0729dfcd0aa36604b2baeddb291e6d
constgen <const.gen@gmail.com>
2018-07-25 11:46:43 +0300

    Fix ESLint violations

--- src/get-ips.js
+++ src/get-ips.js
@@ -1,45 +1,44 @@
-'use strict';
-
 let os = require('os');
 
-function concat(arrayA, arrayB) {
-    return arrayA.concat(arrayB);
+function concat (arrayA, arrayB) {
+	return arrayA.concat(arrayB);
 }
 
-function getFrom(object) {
-    return function (key) {
-        return object[key];
-    };
+function getFrom (object) {
+	return function (key) {
+		return object[key];
+	};
 }
 
-function isIPv4(iface) {
-    return iface.family === 'IPv4';
+function isIPv4 (iface) {
+	return iface.family === 'IPv4';
 }
 
-function getIP(iface) {
-    return iface.address;
+function getIP (iface) {
+	return iface.address;
 }
 
-function getHostName(){
+function getHostName () {
 	try {
 		return os.hostname();
 	}
-	catch(err) {
+	catch (err) {
 		return;
 	}
 }
 
-module.exports = function getIPs() {
+module.exports = function getIPs () {
 	let netInterfaces = os.networkInterfaces();
-	let hostname = getHostName()
-    let IPs = Object.keys(netInterfaces)
+	let hostname = getHostName();
+	let IPs = Object.keys(netInterfaces)
         .map(getFrom(netInterfaces))
         .reduce(concat)
         .filter(isIPv4)
 		.map(getIP);
+
 	if (hostname) {
 		IPs.push(hostname);
 	}
-    IPs.push('localhost');
-    return IPs;
+	IPs.push('localhost');
+	return IPs;
 };
--- src/ip.js
+++ src/ip.js
@@ -1,13 +1,11 @@
-'use strict';
-
 let getIPs = require('./get-ips');
 
 module.exports = {
 	locals: ['127.0.0.1', 'localhost'],
-	get all(){
-		return getIPs()
+	get all () {
+		return getIPs();
 	},
-	isLocal: function(ip) {
-		return module.exports.locals.indexOf(ip) >= 0
+	isLocal (ip) {
+		return module.exports.locals.indexOf(ip) >= 0;
 	}
-}
+};
